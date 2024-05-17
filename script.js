5f13a29a9c4bd7704531785b1009aba43b3ec9c5
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-05-18 00:18:19 +0300

    write the showTask in the js file

--- script.js
+++ script.js
@@ -30,3 +30,7 @@ listContainer.addEventListener("click", function (e) {
 function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
 }
+function showTack() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTack();
